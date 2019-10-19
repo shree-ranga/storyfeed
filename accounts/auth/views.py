39aba16206259ec5700f39ea8e42ff3193303945
Shree Ranga Raju <shreerangaraju@gmail.com>
2019-10-19 03:51:49 -0400

    finish login and register view

---
+++ accounts/auth/views.py
@@ -0,0 +1,39 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+
+from .serializers import LoginSerializer, LogoutSerializer, RegisterSerializer
+
+
+class RegisterAPI(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            user = serializer.instance
+            token = Token.objects.create(user=user)
+            return Response(
+                {"token": token.key, "id": user.id}, status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPI(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serializer = LoginSerializer(data=data)
+        if serializer.is_valid():
+            user = serializer.validated_data.get("user")
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(
+                {"token": token.key, "id": user.id}, status=status.HTTP_200_OK
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
