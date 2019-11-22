f6579e58d8c77448aed6a308d5b4d038fe6ad604
Shree Ranga Raju <shreerangaraju@gmail.com>
2019-11-21 22:41:15 -0500

    create item and like model

--- items/models.py
+++ items/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Item(models.Model):
+    # choices = (("portrait", "pr"), ("landscape", "ln"), ("square", "sq"))
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="item_by"
+    )
+    item = models.ImageField(null=True, blank=True)
+    caption = models.TextField(max_length=150, null=True, blank=True)
+    likes_count = models.PositiveIntegerField(default=0)
+    comments_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return f"{self.id} by {self.user.username}"
+
+
+class Like(models.Model):
+    item = models.ForeignKey("Item", on_delete=models.CASCADE, related_name="item_like")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="liked_by"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("item", "user")
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.item.id}"
--- storyboard/settings.py
+++ storyboard/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     # local
     "accounts",
+    "items",
     "play",
 ]
 
