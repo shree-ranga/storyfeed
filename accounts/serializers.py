7063691b26f20c91f9669fcf9c874567f7d6220b
Shree Ranga Raju <shreerangaraju@gmail.com>
2020-01-11 02:10:07 -0500

    update user info serializer complete

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -44,11 +44,35 @@ class ProfileDetailSerializer(serializers.ModelSerializer):
 class UserDetailSerializer(serializers.ModelSerializer):
     full_name = serializers.SerializerMethodField()
     profile = ProfileDetailSerializer(required=False, many=False)
+    update_full_name = serializers.CharField(required=False)
 
     class Meta:
         model = User
-        fields = ["id", "username", "full_name", "profile"]
+        fields = ["id", "username", "full_name", "profile", "update_full_name"]
         read_only_fields = ["id"]
+        extra_kwargs = {"username": {"required": False}}
+
+    def to_internal_value(self, data):
+        ret = super().to_internal_value(data)
+        update_full_name = ret.pop("update_full_name", None)
+        if update_full_name:
+            split_name = update_full_name.rsplit(None, 1)
+            ret["first_name"] = split_name[0]
+            ret["last_name"] = split_name[1]
+        return ret
+
+    def update(self, instance, validated_data):
+        profile_data = validated_data.pop("profile", None)
+        instance.username = validated_data.get("username", instance.username)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        if profile_data:
+            profile = Profile.objects.get(user=instance)
+            profile.bio = profile_data.get("bio", profile.bio)
+            profile.save()
+            instance.profile = profile
+        instance.save()
+        return instance
 
     def get_full_name(self, obj):
         return str(obj.first_name + " " + obj.last_name)
