349b5fa6db71bfa42b053c86f393c7045d00a427
Shree Ranga Raju <shreerangaraju@gmail.com>
2020-01-22 23:29:05 -0500

    notification bug fixes

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 
+from push_notifications.api.rest_framework import APNSDeviceAuthorizedViewSet
+
 from .views import NotificationListView
 
+# in-app notification urls
 urlpatterns = [
     path(
         "user-notifications/", NotificationListView.as_view(), name="user_notifications"
     )
 ]
 
+# push notification urls
+urlpatterns += [
+    path(
+        "push-notifications/device/apns/",
+        APNSDeviceAuthorizedViewSet.as_view({"post": "create"}),
+        name="create_apns_device",
+    )
+]
--- notifications/views.py
+++ notifications/views.py
@@ -3,11 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from .models import Notification
-from .serializers import NotificationSerializer
+from .serializers import NotificationListSerializer
 
 
 class NotificationListView(APIView):
     def get(self, request, *args, **kwargs):
         queryset = Notification.objects.filter(receiver=request.user)
-        serializer = NotificationSerializer(queryset, many=True)
+        serializer = NotificationListSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
